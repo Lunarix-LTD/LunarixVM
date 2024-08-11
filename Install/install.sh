c9685518020de9feb26f0a7f7d674488cf0a1fee
Nezixyan <146237851+Nezixyan@users.noreply.github.com>
2024-08-11 23:25:25 +0300

    Add files via upload

---
+++ Install/install.sh
@@ -0,0 +1,20 @@
+clear
+echo "------------------------------------"
+echo " WELCOME TO LunarixVM Provider Installer INSTALLER "
+echo "------------------------------------"
+echo "Installing nodejs@16"
+sudo apt update -y
+curl -s https://deb.nodesource.com/setup_16.x | sudo bash
+sudo apt install nodejs -y
+echo "Installing qemu"
+sudo apt-get update -y
+sudo apt-get install qemu  -y
+clear
+node lunarix-provider.js
+cd ..
+npm install
+clear
+echo ------------------------------------
+echo The installation is complete, you can go to the main directory and run npm start
+echo ------------------------------------
+npm start
